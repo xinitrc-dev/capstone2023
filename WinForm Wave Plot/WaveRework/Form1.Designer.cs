1a34ad1cc8f53cc7f62b42e500544386204f8b34
Armand9999 <82055216+Armand9999@users.noreply.github.com>
2023-03-26 12:50:00 -0400

    WinForms Wave Plot

---
+++ WinForm Wave Plot/WaveRework/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WaveRework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Location = new System.Drawing.Point(14, 56);
+            this.formsPlot1.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(772, 382);
+            this.formsPlot1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start Recording";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(660, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop Recording";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.formsPlot1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ScottPlot.FormsPlot formsPlot1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WinForm Wave Plot/WaveRework/Form1.cs
@@ -0,0 +1,94 @@
+﻿using ScottPlot;
+using System;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace WaveRework
+{
+    public partial class Form1 : Form
+    {
+        private WaveIn waveIn;
+        private Plot plot;
+
+        // Define the intensity interval thresholds
+        private const double QuietThreshold = 0.1;
+        private const double LoudThreshold = 0.9;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializePlot();
+        }
+
+        private void InitializePlot()
+        {
+         
+            plot = new Plot(800, 400);
+            formsPlot1.Plot.YLabel("Amplitude");
+            formsPlot1.Plot.XLabel("Time (s)");
+            formsPlot1.Refresh();
+            formsPlot1.Render();
+        }
+
+        private void StartCapturingAudio()
+        {
+            int blockSize = 1024;
+            int sampleRate = 44100; // or the sample rate of your audio input device
+            double[] samples = new double[blockSize];
+
+            waveIn = new WaveIn();
+            waveIn.BufferMilliseconds = blockSize * 1000 / sampleRate;
+            waveIn.WaveFormat = new WaveFormat(sampleRate, 16, 1);
+            waveIn.DataAvailable += (sender, e) =>
+            {
+                byte[] buffer = e.Buffer;
+                int bytesPerSample = waveIn.WaveFormat.BitsPerSample / 8;
+                int sampleCount = e.BytesRecorded / bytesPerSample;
+
+                double sumOfSquares = 0;
+                for (int i = 0; i < sampleCount; i++)
+                {
+                    double sampleValue = BitConverter.ToInt16(buffer, i * bytesPerSample) / 32768.0; // convert to a double value between -1 and 1
+                    sumOfSquares += sampleValue * sampleValue;
+                    samples[i] = sampleValue;
+                }
+
+                double rmsAmplitude = Math.Sqrt(sumOfSquares / sampleCount);
+                string intensity;
+                if (rmsAmplitude < QuietThreshold)
+                {
+                    intensity = "Quiet";
+                }
+                else if (rmsAmplitude > LoudThreshold)
+                {
+                    intensity = "Loud";
+                }
+                else
+                {
+                    intensity = "Normal";
+                }
+
+                formsPlot1.Plot.Clear();
+                formsPlot1.Plot.PlotSignal(samples, sampleRate);
+                formsPlot1.Plot.Title($"Intensity: {intensity} (RMS Amplitude: {rmsAmplitude:F3})");
+                formsPlot1.Render();
+            };
+            waveIn.StartRecording();
+        }
+
+        private void StopCapturingAudio()
+        {
+            waveIn.StopRecording();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartCapturingAudio();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StopCapturingAudio();
+        }
+    }
+}
